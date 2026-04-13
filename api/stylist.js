fb77ab3d1beae7dbc4adf57f0c83c8a9147cd3ea
orinkashi <omaressiass45@gmail.com>
2026-04-13 22:24:21 +0100

    fix export

--- api/stylist.js
+++ api/stylist.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
